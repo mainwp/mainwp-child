6fec46495293543b872fb11e3ccc523960b0318b
ruben- <ruben.schollaert@verticon.be>
2014-05-19 22:58:10 +0200

    new version nr

--- mainwp-child.php
+++ mainwp-child.php
@@ -5,7 +5,7 @@
   Description: Child Plugin for MainWP. The plugin is used so the installed blog can be securely managed remotely by your network. Plugin documentation and options can be found here http://docs.mainwp.com
   Author: MainWP
   Author URI: http://mainwp.com
-  Version: 0.29.4
+  Version: 0.29.5
  */
 header('X-Frame-Options: ALLOWALL');
 //header('X-Frame-Options: GOFORIT');
