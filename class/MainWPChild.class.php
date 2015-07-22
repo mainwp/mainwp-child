68f1ee6e71e83fdda3bdbc3c66fcccb7dd579dd4
ruben- <ruben.schollaert@verticon.be>
2015-07-22 20:47:00 +0200

    update version

--- class/MainWPChild.class.php
+++ class/MainWPChild.class.php
@@ -11,7 +11,7 @@ include_once(ABSPATH . '/wp-admin/includes/plugin.php');
 
 class MainWPChild
 {
-    private $version = '2.0.21';
+    private $version = '2.0.22';
     private $update_version = '1.2';
 
     private $callableFunctions = array(
