bd9d3b4ee125ee88eebab8e624a58262ec6a6b62
Luke Cavanagh <lukecav@users.noreply.github.com>
2017-05-22 10:22:35 -0600

    Create uninstall.php

---
+++ uninstall.php
@@ -0,0 +1,12 @@
+
+<?php
+/**
+ * MainWP Child Uninstall
+ *
+ * Uninstalling mainwp child deletes options.
+ *
+ * @author      mainwp
+ * @category    Core
+ * @package     mainwp-child/Uninstaller
+ * @version     3.4
+ */
