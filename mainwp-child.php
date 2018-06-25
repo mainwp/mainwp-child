4b1ab700fc25d1cf0339223563f1aa9e095d280f
Christian Foellmann <foellmann@foe-services.de>
2018-06-25 10:19:36 +0200

    remove __autoload()

--- mainwp-child.php
+++ mainwp-child.php
@@ -33,13 +33,7 @@ function mainwp_child_autoload( $class_name ) {
 	}
 }
 
-if ( function_exists( 'spl_autoload_register' ) ) {
-	spl_autoload_register( 'mainwp_child_autoload' );
-} else {
-	function __autoload( $class_name ) {
-		mainwp_child_autoload( $class_name );
-	}
-}
+spl_autoload_register( 'mainwp_child_autoload' );
 
 $mainWPChild = new MainWP_Child( WP_PLUGIN_DIR . DIRECTORY_SEPARATOR . plugin_basename( __FILE__ ) );
 register_activation_hook( __FILE__, array( $mainWPChild, 'activation' ) );
