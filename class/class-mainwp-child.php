45644888cff2d2f50bebddca7b15ab4c8049f1b1
Bogdan Rapaić <bogdan@mainwp.com>
2021-10-20 14:21:20 +0200

    Updated: version number

--- class/class-mainwp-child.php
+++ class/class-mainwp-child.php
@@ -33,7 +33,7 @@ class MainWP_Child {
 	 *
 	 * @var string MainWP Child plugin version.
 	 */
-	public static $version = '4.1.7.1';
+	public static $version = '4.1.8';
 
 	/**
 	 * Private variable containing the latest MainWP Child update version.
