85b539a39fa49ba2553148a0a24e97a5f0cbb5bb
codefactor-io <support@codefactor.io>
2021-11-03 12:06:33 +0000

    [CodeFactor] Apply fixes to commit e824351
    [ci skip] [skip ci]

--- class/class-mainwp-child-posts.php
+++ class/class-mainwp-child-posts.php
@@ -215,11 +215,11 @@ class MainWP_Child_Posts {
 			$outPost['dts'] = strtotime( $post->post_date_gmt );
 		}
 
-		$usr               = get_user_by( 'id', $post->post_author );
-		$outPost['author'] = ! empty( $usr ) ? $usr->user_nicename : 'removed';
+		$usr                    = get_user_by( 'id', $post->post_author );
+		$outPost['author']      = ! empty( $usr ) ? $usr->user_nicename : 'removed';
 		$outPost['authorEmail'] = ! empty( $usr ) ? $usr->user_email : 'removed';
-		$categoryObjects   = get_the_category( $post->ID );
-		$categories        = '';
+		$categoryObjects        = get_the_category( $post->ID );
+		$categories             = '';
 		foreach ( $categoryObjects as $cat ) {
 			if ( '' !== $categories ) {
 				$categories .= ', ';
