0b8f66fc09c8a0145666c165509a9aef9f70bb37
Thang Hoang Van <thang@mainwp.com>
2023-03-31 00:53:29 +0700

    Fixed: JS conflict caused by the Hide Jetpack plugin feature

--- class/class-mainwp-child.php
+++ class/class-mainwp-child.php
@@ -33,7 +33,7 @@ class MainWP_Child {
 	 *
 	 * @var string MainWP Child plugin version.
 	 */
-	public static $version = '4.4.0.3';
+	public static $version = '4.4.0.4';
 
 	/**
 	 * Private variable containing the latest MainWP Child update version.
