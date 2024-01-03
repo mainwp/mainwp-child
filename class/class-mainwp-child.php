ce41bc55135f30aa39375b03c25dfee7a52f1f68
Thang Hoang Van <thang@mainwp.com>
2024-01-03 20:11:47 +0700

    Updated: readme and version number

--- class/class-mainwp-child.php
+++ class/class-mainwp-child.php
@@ -33,7 +33,7 @@ class MainWP_Child {
 	 *
 	 * @var string MainWP Child plugin version.
 	 */
-	public static $version = '4.6-RC1';
+	public static $version = '4.6';
 
 	/**
 	 * Private variable containing the latest MainWP Child update version.
