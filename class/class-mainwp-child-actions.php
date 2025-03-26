8b7e6cc073f702fcf74f97e57211da87993bf145
thanghv <thang@mainwp.com>
2025-03-27 01:19:08 +0700

    Fix: for cli error with disabled function posix_getpwuid

--- class/class-mainwp-child-actions.php
+++ class/class-mainwp-child-actions.php
@@ -785,7 +785,7 @@ class MainWP_Child_Actions { //phpcs:ignore -- NOSONAR - multi method.
             'agent'           => (string) $agent,
         );
 
-        if ( 'wp_cli' === $agent && function_exists( 'posix_getuid' ) ) {
+        if ( 'wp_cli' === $agent && is_callable( 'posix_getuid' ) && is_callable( 'posix_getpwuid' ) ) {
             $uid       = posix_getuid();
             $user_info = posix_getpwuid( $uid );
 
