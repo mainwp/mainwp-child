9755c9cea50ffec2aada569837c15e2ea57dffb3
thanghv <thang@mainwp.com>
2025-08-12 22:17:47 +0700

    Update: readme and version number

--- mainwp-child.php
+++ mainwp-child.php
@@ -12,7 +12,7 @@
  * Author: MainWP
  * Author URI: https://mainwp.com
  * Text Domain: mainwp-child
- * Version: 5.4.0.11
+ * Version: 5.4.0.12
  * Requires at least: 5.4
  * Requires PHP: 7.4
  */
