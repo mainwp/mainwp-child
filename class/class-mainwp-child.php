4a6bbac2539f5f64b8ad69b4d04341c0a24dfbb6
Bogdan Rapaić <bogdan@mainwp.com>
2025-09-30 22:03:28 +0200

    Bump version to 5.5-er3
    Updated plugin version references in class-mainwp-child.php, mainwp-child.php, and readme.txt from 5.5-beta2 to 5.5-er3 for the new release.

--- class/class-mainwp-child.php
+++ class/class-mainwp-child.php
@@ -30,7 +30,7 @@ class MainWP_Child {
      *
      * @var string MainWP Child plugin version.
      */
-    public static $version = '5.5-beta2'; // NOSONAR - not IP.
+    public static $version = '5.5-er3'; // NOSONAR - not IP.
 
     /**
      * Private variable containing the latest MainWP Child update version.
